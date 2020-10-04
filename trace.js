19c6572745f4517af7286c4a94044c09e37b7a66
Mukhtar <mukhtartanimugarba@gmail.com>
2020-10-04 21:13:50 +0100

    Files updated

---
+++ trace.js
@@ -0,0 +1,7 @@
+function trace(geometries, rayOrigin, rayDirection, depth) {
+    var geoNear = undefined;
+
+    for (var i = 0; i < geometries.lengtg; i++) {
+        var result = geometries[i].intersect(rayorig, raydir)
+    }
+}
