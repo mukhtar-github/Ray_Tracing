69b837656101d73735817be87b419bdb4026f6f0
Mukhtar <mukhtartanimugarba@gmail.com>
2020-10-05 03:24:33 +0100

    Files updated

--- trace.js
+++ trace.js
@@ -2,6 +2,17 @@ function trace(geometries, rayOrigin, rayDirection, depth) {
     var geoNear = undefined;
 
     for (var i = 0; i < geometries.lengtg; i++) {
-        var result = geometries[i].intersect(rayorig, raydir)
+        var result = geometries[i].intersect(rayorig, raydir);
+        // intersection distance is closer than origin -> save dist & geometry
     }
+    if (geoNear === undefined) { return BACKGROUND_COLOR; }
+    if (geoNear.material.transparent || geoNear.material.reflective) {
+        // compute reflection ray - recurse
+        var reflectionColor = trace(geometries, reflOrigin, reflDirection, depth + 1);
+        // compute refraction ray - recurse
+        var refractionColor = trace(geometries, refrOrigin, refrDirection, depth + 1);
+        var Kr = fresnel(...)
+
+        return reflectionColor * Kr + refractionColor * (1 - Kr);
+    } else { ... }
 }
